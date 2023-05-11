a536375ebad1c3ef411c7b9626e722d8e9f4de40
Shapo <ShaboJia@outlook.com>
2023-05-11 16:55:21 +0800

    modify script metadata, name, author etc

--- src/first.js
+++ src/first.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         炸号微博备份
-// @namespace    https://dun.mianbaoduo.com/@fun
-// @version      0.7
-// @description  炸号微博一键备份
-// @author       fun
+// @name         微博备份
+// @namespace    https://github.com/Shapooo/
+// @version      0.0
+// @description  微博数据下载备份
+// @author       Shapooo
 // @match        *://weibo.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=weibo.com
 // @grant        none
