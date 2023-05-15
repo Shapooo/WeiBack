279c620a0ffbe4185c4b989a6a82d17a71b92aaf
Shapo <ShaboJia@outlook.com>
2023-05-15 23:55:20 +0800

    version 0.2

--- src/first.js
+++ src/first.js
@@ -1,12 +1,15 @@
 // ==UserScript==
 // @name         微博备份
 // @namespace    https://github.com/Shapooo/
-// @version      0.1
+// @version      0.2
 // @description  微博数据下载备份
 // @author       Shapooo
+// @homepageURL  https://github.com/Shapooo/WeiBack
 // @match        *://weibo.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=weibo.com
 // @grant        none
 // @license      GPL
+// @note         2023-05-12 0.1 完成最基本的功能，能够拉取原始json数据文件进行备份
+// @note         2023-05-15 0.2 完成html格式保存功能
 // ==/UserScript==
 "use strict";
