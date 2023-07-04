d4a5057f546db262735d19057314cd4827190564
Shapo <ShaboJia@outlook.com>
2023-07-04 22:00:34 +0800

    update version and config files

--- vite.config.ts
+++ vite.config.ts
@@ -7,19 +7,17 @@ export default defineConfig({
     monkey({
         entry: 'src/main.js',
         userscript: {
-            name: {
-                '': 'weiback',
-            },
-            icon: 'https://www.google.com/s2/favicons?sz=64&domain=weibo.com',
+            name: 'weiback',
             namespace: 'https://github.com/Shapooo/',
-            description: {
-                '': '微博数据下载备份',
-            },
+            author: 'Shapooo',
+            description: '微博数据下载备份',
+            homepageURL: 'https://github.com/Shapooo/WeiBack',
             match: [
                 '*://*.weibo.com/*',
             ],
             require: ['https://cdn.bootcdn.net/ajax/libs/jszip/3.10.1/jszip.min.js',
                 'https://cdn.bootcdn.net/ajax/libs/FileSaver.js/2.0.5/FileSaver.min.js'],
+            icon: 'https://www.google.com/s2/favicons?sz=64&domain=weibo.com',
             grant: 'none',
             license: 'GPL',
         },
