cd5a65bf34010ed47a7b3d08cb4ff1b905677cc4
Shapo <ShaboJia@outlook.com>
2023-09-22 20:46:35 +0800

    add checkbox to not download picture

--- src/fetch.js
+++ src/fetch.js
@@ -4,6 +4,7 @@ import {showTip} from './main.js';
 export {fetchAllPosts};
 
 const domain = window.location.host;
+const downloadPic = true;
 const HTML_GEN_TEMP = '<html lang="zh-CN"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width,initial-scale=1"><style>body{background-color:#f1f2f5}.bk-post-wrapper{border-radius:4px;background:#fff;width:700px;margin:8px auto;padding:10px 0}.bk-poster{display:flex;align-items:center;vertical-align:middle;height:60px}.bk-poster-avatar,.bk-retweeter-avatar{height:60px;width:60px;border-radius:50%;margin:auto 8px}.bk-retweeter-avatar{height:40px;width:40px}.bk-post-text{background:#fff}.bk-content{margin:8px 24px 8px 76px;font-size:15px;line-height:24px}.bk-retweeter{display:flex;align-items:center;vertical-align:middle;height:40px;margin:8px 24px 8px 76px}.bk-retweet{background:#f9f9f9;padding:2px 0}.bk-pic{max-width:600px;max-height:400px;margin:auto}.bk-poster-name,.bk-retweeter-name{color:#000;font-weight:700;text-decoration:none;font-family:Arial,Helvetica,sans-serif}.bk-poster-name:hover,.bk-retweeter-name:hover{color:#eb7350}.bk-icon-link{height:20px;filter:sepia(100%) saturate(3800%) contrast(75%);vertical-align:middle;margin-bottom:4px}.bk-link,.bk-user{color:#eb7350;text-decoration:none}.bk-link:hover,.bk-user:hover{text-decoration:underline}.bk-emoji{height:20px;vertical-align:middle;margin-bottom:4px}.bk-create-detail{font-size:10px;color:#939393}</style><title>微博备份</title></head><body></body></html>';
 const STATUSES_MY_MICRO_BLOG_API = `https://${domain}/ajax/statuses/mymblog`;
 const STATUSES_CONFIG_API = `https://${domain}/ajax/statuses/config`;
@@ -27,7 +28,7 @@ async function fetchPostMeta(uid = 0, page = 1, type = 'myblog') {
   return data;
 }
 
-async function fetchAllPosts(type = 'myblog', range, uid) {
+async function fetchAllPosts(type = 'myblog', range, downloadPic, uid) {
   console.log(`fetching ${type} post ${range}`);
 
   const storage = {
@@ -68,17 +69,19 @@ async function fetchAllPosts(type = 'myblog', range, uid) {
     }
 
     allPageData.push(await generateHTMLPage(data, storage));
-    for (const url of storage.picUrls) {
-      if (resources.get(url)) {
-        continue;
-      }
-      const blob = storage.cache.get(url);
-      if (blob) {
-        resources.set(url, blob);
-      } else {
-        const blob = await fetchPic(url);
-        storage.cache.set(url, blob);
-        resources.set(url, blob);
+    if (downloadPic) {
+      for (const url of storage.picUrls) {
+        if (resources.get(url)) {
+          continue;
+        }
+        const blob = storage.cache.get(url);
+        if (blob) {
+          resources.set(url, blob);
+        } else {
+          const blob = await fetchPic(url);
+          storage.cache.set(url, blob);
+          resources.set(url, blob);
+        }
       }
     }
     storage.picUrls.clear();
